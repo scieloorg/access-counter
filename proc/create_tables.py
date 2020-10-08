9d7a013ce4fc01a30e887a7e0b71a7c73cc98a22
Rafael J P D <rafael.pezzuto@gmail.com>
2020-10-08 10:52:48 -0300

    Adiciona processo para executar criação de tabelas

---
+++ proc/create_tables.py
@@ -0,0 +1,31 @@
+import argparse
+import datetime
+import logging
+import sys
+sys.path.append('..')
+
+from utils import db_tools
+
+
+logging.basicConfig(filename='create_tables_' + datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S') + '.log',
+                    level=logging.DEBUG)
+
+
+def main():
+    usage = """Cria tabelas para uso da API SUSHI em relatórios COUNTER."""
+    parser = argparse.ArgumentParser(usage)
+
+    parser.add_argument(
+        '-d',
+        required=True,
+        dest='mariadb_uri',
+        help='String de conexão a base SQL no formato mysql://username:password@host1:port/database'
+    )
+
+    params = parser.parse_args()
+
+    db_tools.create_tables(params.mariadb_uri)
+
+
+if __name__ == '__main__':
+    main()
