9d680255b86c1cbce5e359ff75b895956dae8c17
Rafael J P D <rafael.pezzuto@gmail.com>
2020-10-10 14:33:43 -0300

    Corrige tipologia dos campos para serem compatíveis com aqueles do Matomo

--- utils/sql_declarative.py
+++ utils/sql_declarative.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint, Index, Date, SmallInteger, BigInteger, DateTime, Binary
+from sqlalchemy import Column, ForeignKey, Integer, UniqueConstraint, Index, Date, DateTime
+from sqlalchemy.dialects.mysql import BIGINT, BINARY, INTEGER, TINYINT, VARBINARY, VARCHAR
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -13,13 +14,13 @@ class Journal(Base):
     __table_args__ += (Index('index_print_issn', 'print_issn'),)
     __table_args__ += (Index('index_print_online', 'online_issn'),)
 
-    journal_id = Column(Integer, primary_key=True, autoincrement=True)
-    collection_acronym = Column(String(3), nullable=False)
-    title = Column(String(255), nullable=False)
-    print_issn = Column(String(9), nullable=False)
-    online_issn = Column(String(9), nullable=False)
-    uri = Column(String(255))
-    publisher_name = Column(String(255))
+    journal_id = Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    collection_acronym = Column(VARCHAR(3), nullable=False)
+    title = Column(VARCHAR(255), nullable=False)
+    print_issn = Column(VARCHAR(9), nullable=False)
+    online_issn = Column(VARCHAR(9), nullable=False)
+    uri = Column(VARCHAR(255))
+    publisher_name = Column(VARCHAR(255))
 
 
 class Article(Base):
@@ -27,21 +28,24 @@ class Article(Base):
     __table_args__ = (UniqueConstraint('collection_acronym', 'pid', name='uni_col_pid'),)
     __table_args__ += (Index('index_col_pid', 'collection_acronym', 'pid'),)
 
-    article_id = Column(Integer, primary_key=True, autoincrement=True)
-    collection_acronym = Column(String(3), nullable=False)
-    pid = Column(String(23), nullable=False)
-    fk_journal_id = Column(Integer, ForeignKey('counter_journal.journal_id', name='fk_journal_id'))
+    article_id = Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    collection_acronym = Column(VARCHAR(3), nullable=False)
+    pid = Column(VARCHAR(23), nullable=False)
+
+    journal_id = Column(INTEGER(unsigned=True), ForeignKey('counter_journal.journal_id', name='journal_id'))
     journal = relationship(Journal)
 
 
 class MetricArticle(Base):
     __tablename__ = 'counter_metric_article'
-    __table_args__ = (UniqueConstraint('fk_article_id', 'year_month_day'),)
+    __table_args__ = (UniqueConstraint('article_id', 'year_month_day'),)
     __table_args__ += (Index('index_year_month_day', 'year_month_day'),)
 
-    metric_id = Column(Integer, primary_key=True, autoincrement=True)
-    fk_article_id = Column(Integer, ForeignKey('counter_article.article_id', name='fk_article_id'), nullable=False)
+    metric_id = Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+
+    article_id = Column(INTEGER(unsigned=True), ForeignKey('counter_article.article_id', name='article_id'))
     article = relationship(Article)
+
     year_month_day = Column(Date, nullable=False)
     total_item_requests = Column(Integer, nullable=False)
     total_item_investigations = Column(Integer, nullable=False)
@@ -53,34 +57,37 @@ class LogAction(Base):
     __tablename__ = 'matomo_log_action'
     __table_args__ = (Index('index_type_hash', 'type', 'hash'),)
 
-    idaction = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(4096))
-    hash = Column(Integer, nullable=False)
-    type = Column(SmallInteger, nullable=False)
-    url_prefix = Column(SmallInteger)
+    idaction = Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    name = Column(VARCHAR(4096))
+    hash = Column(INTEGER(unsigned=True), nullable=False)
+    type = Column(TINYINT(unsigned=True))
+    url_prefix = Column(TINYINT(2))
 
 
 class LogVisit(Base):
     __tablename__ = 'matomo_log_visit'
+    __table_args__ = (Index('index_idsite_idvisitor', 'idsite', 'idvisitor'),)
 
-    idvisit = Column(BigInteger, primary_key=True, autoincrement=True)
-    idvisitor = Column(Binary)
-    config_browser_name = Column(String(10))
-    config_browser_version = Column(String(20))
-    location_ip = Column(Binary)
+    idvisit = Column(BIGINT(10, unsigned=True), primary_key=True, autoincrement=True)
+    idsite = Column(INTEGER(10, unsigned=True))
+    idvisitor = Column(BINARY(8), nullable=False)
+    config_browser_name = Column(VARCHAR(10))
+    config_browser_version = Column(VARCHAR(20))
+    location_ip = Column(VARBINARY(16), nullable=False)
 
 
 class LogLinkVisitAction(Base):
     __tablename__ = 'matomo_log_link_visit_action'
-    __table_args__ = (Index('index_visit','idvisit'),)
-    __table_args__ += (Index('index_idsite_servertime', 'idsite', 'server_time'),)
+    __table_args__ = (Index('index_idsite_servertime', 'idsite', 'server_time'),)
+    __table_args__ += (Index('index_idvisit', 'idvisit'),)
 
-    idlink_va = Column(BigInteger, primary_key=True, autoincrement=True)
-    idsite = Column(Integer, nullable=False)
+    idlink_va = Column(BIGINT(10, unsigned=True), primary_key=True, autoincrement=True)
+    idsite = Column(INTEGER(unsigned=True), nullable=False)
+    idvisitor = Column(BINARY(8), nullable=False)
     server_time = Column(DateTime, nullable=False)
 
-    idaction_url = Column(Integer, ForeignKey('matomo_log_action.idaction', name='idaction_url'))
+    idaction_url = Column(INTEGER(unsigned=True), ForeignKey('matomo_log_action.idaction', name='idaction_url'))
     action = relationship(LogAction)
 
-    idvisit = Column(Integer, ForeignKey('matomo_log_visit.idvisit', name='idvisit'), nullable=False)
+    idvisit = Column(BIGINT(10, unsigned=True), ForeignKey('matomo_log_visit.idvisit', name='idvisit'))
     visit = relationship(LogVisit)
