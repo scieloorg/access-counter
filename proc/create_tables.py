bbbf2bdc09e0c0c92fb89c22e62da1c33ad8ae52
Rafael J P D <rafael.pezzuto@gmail.com>
2020-10-10 15:36:42 -0300

    Adiciona logging ao executar criação de tabelas e melhora coleta de exceções

--- proc/create_tables.py
+++ proc/create_tables.py
@@ -1,14 +1,9 @@
 import argparse
-import datetime
 import logging
 import sys
 sys.path.append('..')
 
 from utils import db_tools
-
-
-logging.basicConfig(filename='create_tables_' + datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S') + '.log',
-                    level=logging.DEBUG)
 
 
 def main():
@@ -18,13 +13,27 @@ def main():
     parser.add_argument(
         '-d',
         required=True,
-        dest='matomodb_uri',
+        dest='matomo_db_uri',
         help='String de conexão a base SQL no formato mysql://username:password@host1:port/database'
+    )
+
+    parser.add_argument(
+        '--logging_level',
+        choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET'],
+        dest='logging_level',
+        default='INFO',
+        help='Nível de log'
     )
 
     params = parser.parse_args()
 
-    db_tools.create_tables(params.matomodb_uri)
+    logging.basicConfig(level=params.logging_level)
+
+    logging.info('Criando tabelas COUNTER')
+    db_tools.create_tables(params.matomo_db_uri)
+
+    logging.info('Alterando tabelas Matomo')
+    db_tools.add_foreign_keys_to_table_matomo_log_link_action(params.matomo_db_uri)
 
 
 if __name__ == '__main__':
