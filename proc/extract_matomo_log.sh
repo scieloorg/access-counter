1599c9d2e5eb4b438e8741930d5afeb45b3976e7
Rafael J P D <rafael.pezzuto@gmail.com>
2020-10-23 00:59:02 -0300

    Remove condição idsite pois esta coluna não é um índice e atrasa a query

--- proc/extract_matomo_log.sh
+++ proc/extract_matomo_log.sh
@@ -3,7 +3,6 @@ year=$1
 month=$2
 user=$3
 pass=$4
-idsite=$5
 
 for i in $(seq 1 31); do
     if [ $i -lt 10 ]
@@ -31,5 +30,5 @@ for i in $(seq 1 31); do
         fi
     fi
     echo "Extraindo dados de $start..."
-    mysql -u $user -p matomo --password=$pass -e "SELECT server_time as serverTime, config_browser_name as browserName, config_browser_version as browserVersion, inet_ntoa(conv(hex(location_ip), 16, 10)) as ip, name as actionName from matomo_log_link_visit_action LEFT JOIN matomo_log_visit on matomo_log_visit.idvisit = matomo_log_link_visit_action.idvisit LEFT JOIN matomo_log_action on matomo_log_action.idaction = matomo_log_link_visit_action.idaction_url WHERE matomo_log_visit.idsite = $idsite AND server_time >= '$start' AND server_time < '$end' order by location_ip;" > "results_$start.tsv"
+    mysql -u $user -p matomo --password=$pass -e "SELECT server_time as serverTime, config_browser_name as browserName, config_browser_version as browserVersion, inet_ntoa(conv(hex(location_ip), 16, 10)) as ip, name as actionName from matomo_log_link_visit_action LEFT JOIN matomo_log_visit on matomo_log_visit.idvisit = matomo_log_link_visit_action.idvisit LEFT JOIN matomo_log_action on matomo_log_action.idaction = matomo_log_link_visit_action.idaction_url WHERE server_time >= '$start' AND server_time < '$end' order by location_ip;" > "results_$start.tsv"
 done
