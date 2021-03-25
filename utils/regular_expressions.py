864c2650668c83a7b57713255eb19bea157f8331
Rafael JPD <rafael.pezzuto@gmail.com>
2021-03-25 15:00:55 -0300

    Adiciona expressões regulares associadas ao novo formato de URL

--- utils/regular_expressions.py
+++ utils/regular_expressions.py
@@ -30,3 +30,16 @@ REGEX_JOURNAL_SUBSCRIPTION = r'.*/revistas/(\w*)/[e|i|p]subscrp.htm'
 # Detecta ISSN em URLs relacionadas periódico (serial_sci_serial e serial_sci_issues)
 REGEX_JOURNAL_SCRIPT_SCI_SERIAL = r'.*script_sci_serial.*pid_(\d{4}-\d{3}[\d|x|X]).*'
 REGEX_JOURNAL_SCRIPT_SCI_ISSUES = r'.*script_sci_issues.*pid_(\d{4}-\d{3}[\d|x|X]).*'
+
+# Detecam URLs associadas à versão nova dos sites SciELO
+REGEX_NEW_SCL_JOURNAL_ARTICLE_ABSTRACT = r'.*/j/(\w*)/a/(\w*)/abstract'  # grupo 1 = acrônimo, grupo 2 = PID
+REGEX_NEW_SCL_JOURNAL_ARTICLE = r'.*/j/(\w*)/a/(\w*)'  # grupo 1 = acrônimo, grupo 2 = PID
+REGEX_NEW_SCL_JOURNAL_FEED = r'.*/journal/(\w*)/feed'  # grupo 1 = acrônimo
+REGEX_NEW_SCL_JOURNAL_GRID = r'.*/j/(\w*)/grid'  # grupo 1 = acrônimo
+REGEX_NEW_SCL_JOURNAL_TOC = r'.*/j/(\w*)/i/([\w|\.]*)'  # grupo 1 = acrônimo, grupo 2 = fascículo
+REGEX_NEW_SCL_JOURNAL_AOP = r'.*/j/(\w*)/aop'  # grupo 1 = acrônimo
+REGEX_NEW_SCL_JOURNAL = r'.*/j/(\w*)/'  # grupo 1 = acrônimo
+REGEX_NEW_SCL_JOURNALS_ALFAPHETIC = r'.*/journals/alpha'
+REGEX_NEW_SCL_JOURNALS_THEMATIC = r'.*/journals/thematic'
+REGEX_NEW_SCL_RAW = r'.*/article/ssm/content/raw/'
+REGEX_NEW_SCL_RAW_DETAIL = r'.*/documentstore/([\w|-]*)/(\w*)/([\w|\.]*)' # grupo 1 = ISSN, grupo 2 = código ds, grupo 3 = insumo
