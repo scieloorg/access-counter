cd2f2dc2d91f60b3de1037387e6ee4364a0c4795
Rafael JPD <rafael.pezzuto@gmail.com>
2021-06-25 16:08:03 -0300

    Adiciona módulo para coletar dados de Provedor OAI-PMH de Preprint

---
+++ proc/collect_preprint_dictionary.py
@@ -0,0 +1,64 @@
+import argparse
+import json
+import logging
+import os
+
+from sickle import Sickle
+
+
+DIR_DATA = os.environ.get('DIR_DATA', '/opt/counter/data')
+DIR_DICTIONARIES = os.path.join(DIR_DATA, 'dictionaries')
+OAI_PMH_PREPRINT_ENDPOINT = os.environ.get('OAI_PMH_PREPRINT_ENDPOINT', 'https://preprints.scielo.org/index.php/scielo/oai')
+OAI_METADATA_PREFIX = os.environ.get('OAI_METADATA_PREFIX', 'oai_dc')
+PREPRINT_DICTIONARY_PREFIX = os.environ.get('PREPRINT_DICTIONARY_PREFIX', 'pre-counter-dict-')
+
+
+def parse(record):
+    if len(record.metadata.get('date')) > 0:
+        print(record.header.identifier)
+
+    if len(record.metadata.get('language')) > 0:
+        print(record.header.identifier)
+
+    return {
+        record.header.identifier: {
+            'publication_date': record.metadata.get('date').pop(),
+            'default_language': record.metadata.get('language').pop()
+        }
+    }
+
+
+def save(data, filename):
+    filepath = os.path.join(DIR_DICTIONARIES, filename)
+
+    if not os.path.exists(DIR_DICTIONARIES):
+        os.makedirs(DIR_DICTIONARIES)
+
+    try:
+        json.dump(data, open(filepath, 'w'))
+    except FileExistsError:
+        logging.error('Arquivo %s já existe' % filepath)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--from_date', required=True)
+    params = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO,
+                        format='[%(asctime)s] %(levelname)s %(message)s',
+                        datefmt='%d/%b/%Y %H:%M:%S')
+
+    oai_client = Sickle(endpoint=OAI_PMH_PREPRINT_ENDPOINT, max_retries=3, verify=False)
+    records = oai_client.ListRecords(**{
+        'metadataPrefix': OAI_METADATA_PREFIX,
+        'from': params.from_date
+    })
+
+    logging.info('Obtendo dados do OAI-PMH Preprints para date >= %s' % params.from_date)
+    data = {}
+    for r in records:
+        data.update(parse(r))
+
+    filename = ''.join([PREPRINT_DICTIONARY_PREFIX, params.from_date, '.json'])
+    save(data, filename)
