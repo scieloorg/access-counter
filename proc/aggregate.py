5eafb8e9d6e70e456ed4d0e32f6ee9d198f017ec
Rafael JPD <rafael.pezzuto@gmail.com>
2022-02-01 10:02:40 -0300

    Adiciona nome do novo modelo de agregação em lista de tabelas do comando aggregate

--- proc/aggregate.py
+++ proc/aggregate.py
@@ -2,11 +2,13 @@ import argparse
 import logging
 import os
 import time
+import reverse_geocode
 
 from datetime import datetime, timedelta
 from libs import lib_database, lib_status
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.cursor import LegacyCursorResult
 
 
 STR_CONNECTION = os.environ.get('STR_CONNECTION', 'mysql://user:pass@localhost:3306/matomo')
@@ -15,7 +17,7 @@ LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', 'INFO')
 ENGINE = create_engine(STR_CONNECTION, pool_recycle=1800)
 SESSION_FACTORY = sessionmaker(bind=ENGINE)
 SESSION_BULK_LIMIT = int(os.environ.get('SESSION_BULK_LIMIT', '500'))
-TABLES_TO_UPDATE = os.environ.get('TABLES', 'aggr_article_language_year_month_metric,aggr_journal_language_year_month_metric')
+TABLES_TO_UPDATE = os.environ.get('TABLES', 'aggr_article_language_year_month_metric,aggr_journal_language_year_month_metric,aggr_journal_geolocation_year_month_metric')
 
 
 def _extrac_dates_from_period(period: str):
