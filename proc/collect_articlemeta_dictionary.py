5f8ddd0c08fb106a6b8a628cc718888ad6c37d69
Rafael JPD <rafael.pezzuto@gmail.com>
2022-06-06 18:21:33 -0300

    Cria variáveis de ambiente

--- proc/collect_articlemeta_dictionary.py
+++ proc/collect_articlemeta_dictionary.py
@@ -8,3 +8,37 @@ from datetime import datetime
 from time import sleep
 
 
+DIR_DATA = os.environ.get(
+    'DIR_DATA', 
+    '/app/data'
+)
+
+DIR_DICTIONARIES = os.path.join(
+    DIR_DATA, 
+    'dictionaries'
+)
+
+ARTICLEMETA_ENDPOINT = os.environ.get(
+    'ARTICLEMETA_COLLECT_URL', 
+    'http://articlemeta.scielo.org/api/v1/article/counter_dict/'
+)
+
+ARTICLEMETA_DICTIONARY_PREFIX = os.environ.get(
+    'ARTICLEMETA_COLLECT_DICTIONARY_PREFIX', 
+    'am-counter-dict-'
+)
+
+LOGGING_LEVEL = os.environ.get(
+    'ARTICLEMETA_COLLECT_LOGGING_LEVEL',
+    'INFO'
+)
+
+MAX_RETRIES = int(os.environ.get(
+    'ARTICLEMETA_COLLECT_MAX_RETRIES',
+    5
+))
+
+SLEEP_TIME = int(os.environ.get(
+    'ARTICLEMETA_COLLECT_URL_SLEEP_TIME',
+    30
+))
