f37ae6e7af88d2cc0d1fae0561348f636b623337
Rafael JPD <rafael.pezzuto@gmail.com>
2022-07-07 17:41:15 -0300

    Atualiza setup.py para 0.7.2

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ install_requires = [
 
 setup(
     name="scielo-usage-counter",
-    version='0.7.1',
+    version='0.7.2',
     description="The SciELO COUNTER Tools",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
